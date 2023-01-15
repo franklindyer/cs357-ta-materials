e8dc2b0220905c3ad6bbc5e4ceeb830e890e6403
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2023-01-15 14:37:55 -0700

    Add presentations for week 1.

---
+++ presentations/week 1/basic_logistics/.dir-locals.el
@@ -0,0 +1,1 @@
+((latex-mode (TeX-master . "/Users/typesarespaces/Documents/GithubProjects/phd-thesis/Documents/Semesters/2023/Spring/TA-CS-357/cs357-ta-materials/presentations/week 1/basic_logistics/main.tex")))
