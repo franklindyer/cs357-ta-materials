0b6f39fcc0561d98f7ac5d2d9819d44d8beafed8
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2023-03-28 15:55:17 -0600

    Add skeleton code for Hw4.

---
+++ haskell/skeletons/hw4-skeleton.hs
@@ -0,0 +1,50 @@
+-- Exercise 1
+stutter :: [a] -> [a]
+stutter _ = undefined
+
+-- Exercise 2
+compress :: Eq a => [a] -> [a]
+compress _ = undefined
+
+-- Exercise 3
+findIndices :: Num b => (a -> Bool) -> [a] -> [b]
+findIndices _ _ = undefined
+
+intersect :: Eq a => [a] -> [a] -> [a]
+intersect _ _ = undefined
+
+-- Exercise 4
+isPrefixOf :: Eq a => [a] -> [a] -> Bool
+isPrefixOf _ _ = undefined
+
+-- Exercise 5
+isSuffixOf :: Eq a => [a] -> [a] -> Bool
+isSuffixOf _ _ = undefined
+
+-- Exercise 6
+dot :: Num a => [a] -> [a] -> a
+dot _ _ = undefined
+
+-- Exercise 7
+increasing :: Ord a => [a] -> Bool
+increasing _ = undefined
+
+-- Exercise 8
+decimate :: [a] -> [a]
+decimate _ = undefined
+
+-- Exercise 9
+encipher :: Eq a => [a] -> [a] -> [a] -> [a]
+encipher _ _ _ = undefined
+
+-- Exercise 10
+prefixSum :: Num a => [a] -> [a]
+prefixSum _ = undefined
+
+-- Exercise 11
+select :: (t -> Bool) -> [t] -> [a] -> [a]
+select _ _ _ = undefined
+
+-- Exercise 12
+numbers :: [Integer] -> Integer
+numbers _ = undefined 
